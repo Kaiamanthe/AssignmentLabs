83d85e19724119c759a01d5a89dbc5154bf94f64
Khai Ha <Khaiha265@gmail.com>
2025-04-27 18:29:21 -0700

    +Added AssignmentService to core ClassLibrary +Added Private List to AssignmentService +Added AddAssignment Test +Added AddAssignment Method to Service

---
+++ AssignmentLabs/AssignmentService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentLibrary
+{
+    public class AssignmentService
+    {
+        private readonly List<Assignment> assignments = new();
+
+        public void AddAssignment(Assignment assignment)
+        {
+            assignments.Add(assignment);
+        }
+
+        public List<Assignment> ListAll()
+        {
+            return new List<Assignment>(assignments);
+        }
+
+        public List<Assignment> ListIncomplete()
+        {
+            // TODO: Return only assignments where IsCompleted is false
+            throw new NotImplementedException();
+        }
+    }
+}
